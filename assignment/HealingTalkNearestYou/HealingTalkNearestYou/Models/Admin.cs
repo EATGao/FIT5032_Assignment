10506526998b06545277f3f2b741ed2f2a1a10e1
Gao Yurong <ygao0096@student.monash.edu>
2022-08-22 11:54:57 +0800

    entities initial commit

---
+++ assignment/HealingTalkNearestYou/HealingTalkNearestYou/Models/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealingTalkNearestYou.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
---
+++ assignment/HealingTalkNearestYou/HealingTalkNearestYou/Models/Psychologist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealingTalkNearestYou.Models
+{
+    public class Psychologist
+    {
+        [Key]
+        public int PyschologistId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public string SelfDescription { get; set; }
+    }
+}
