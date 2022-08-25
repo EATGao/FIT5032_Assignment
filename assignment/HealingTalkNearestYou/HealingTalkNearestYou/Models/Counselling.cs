da94188e6385ec6a63413ed9f43fc88ff05c9394
Gao Yurong <ygao0096@student.monash.edu>
2022-08-25 16:52:29 +0800

    add Counselling entity, db.mdf and corresponding Conext file

---
+++ assignment/HealingTalkNearestYou/HealingTalkNearestYou/Models/Counselling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealingTalkNearestYou.Models
+{
+    public class Counselling
+    {
+        [Key]
+        public int CounsellingId { get; set; }
+
+        [Required]
+        public Psychologist ThePsychologist { get; set; }
+
+        [Required]
+        public DateTime CousellingDateTime { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        public int Patientid { get; set; }
+    }
+}
