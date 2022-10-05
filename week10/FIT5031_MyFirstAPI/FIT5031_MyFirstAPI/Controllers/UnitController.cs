0e46b8364be01d95a7d3456c6f3359faa0e75b42
Gao Yurong <ygao0096@student.monash.edu>
2022-10-05 10:42:03 +0800

    week 10 task done

---
+++ week10/FIT5031_MyFirstAPI/FIT5031_MyFirstAPI/Controllers/UnitController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIT5031_MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitsController : ControllerBase
+    {
+
+        private readonly ILogger<UnitsController> _logger;
+
+        public UnitsController(ILogger<UnitsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUnit")]
+        public IEnumerable<Unit> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Unit
+            {
+                Id = index,
+                UnitCode = index,
+                UnitName = "Name " + index
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ week10/FIT5031_MyFirstAPI/FIT5031_MyFirstAPI/Unit.cs
@@ -0,0 +1,11 @@
+﻿namespace FIT5031_MyFirstAPI
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+
+        public int UnitCode { get; set; }
+
+        public string? UnitName { get; set; }
+    }
+}
